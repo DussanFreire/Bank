1b565ab9e911a2272c4b6d1b186c4d06bb9a2d5f
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2021-11-14 21:50:50 -0400

    Add test testFindArrayListOfISortableInt

--- Tests/SearchTest.java
+++ Tests/SearchTest.java
@@ -18,5 +18,15 @@ class SearchTest {
 		central.createBank("java", 300);
 		assertEquals(1, searcher.findPosition(central.getBankList(), 100));
 	}
+	
+	@Test
+	void testFindArrayListOfISortableInt() throws Exception {
+		Search searcher = new Linear();
+		CentralBank central = new CentralBank();
+		central.createBank("test", 200);
+		central.createBank("unit", 100);
+		central.createBank("java", 300);
+		assertEquals("java", searcher.find(central.getBankList(), 300).getName());
+	}
 
 }
