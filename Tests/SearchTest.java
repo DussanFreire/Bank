31b3f4d84917ceb3b9d75b963765a7efc16a45f6
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2021-11-14 21:43:39 -0400

    Add test to Search

---
+++ Tests/SearchTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchTest {
+
+	@Test
+	void testSearch() {
+		assertNotNull(new Linear());
+	}
+
+}
