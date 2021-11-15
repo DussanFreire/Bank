7f83142e1a07c9e1c6e1add6cf65cfda8546a695
Victor Hugo Montano <victor.montano@assuresoftbolivia.com>
2021-11-14 21:44:42 -0400

    Add test testFindPosition

--- Tests/SearchTest.java
+++ Tests/SearchTest.java
@@ -8,5 +8,15 @@ class SearchTest {
 	void testSearch() {
 		assertNotNull(new Linear());
 	}
+	
+	@Test
+	void testFindPosition() throws Exception {
+		Search searcher = new Linear();
+		CentralBank central = new CentralBank();
+		central.createBank("test", 200);
+		central.createBank("unit", 100);
+		central.createBank("java", 300);
+		assertEquals(1, searcher.findPosition(central.getBankList(), 100));
+	}
 
 }
